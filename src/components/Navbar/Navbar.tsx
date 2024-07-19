a61a643931174219c93a6653424f421d3e5318f2
innerlize <alan.dev@outlook.kr>
2024-07-19 18:32:10 -0300

    Create 'Navbar' component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,13 @@
+import { useMediaQuery } from 'react-responsive';
+import { MobileNavbar } from './MobileNavbar';
+import { DesktopNavbar } from './DesktopNavbar';
+
 const Navbar: React.FC = () => {
+	const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1279px)' });
+
 	return (
-		<nav>
-			<div>Navbar</div>
+		<nav className='w-full h-[90px] bg-primary md:h-[120px] xl:h-[72px]'>
+			{isTabletOrMobile ? <MobileNavbar /> : <DesktopNavbar />}
 		</nav>
 	);
 };
