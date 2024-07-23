44707b2a9a89080e1fdde7fb61e54be056cd0cc3
innerlize <alan.dev@outlook.kr>
2024-07-22 22:57:46 -0300

    Create components for each type of skill

--- src/components/About/SkillsContainer.tsx
+++ src/components/About/SkillsContainer.tsx
@@ -10,3 +10,27 @@ export const SkillsContainer: React.FC = () => {
 		</div>
 	);
 };
+
+const TechnicalSkills: React.FC = () => {
+	return (
+		<div
+			data-test='technical-skills'
+			className='flex flex-col gap-10 xl:flex-1'>
+			{skills.technical.map((skill, index) => (
+				<SkillTip key={index} typeOfSkill='Technical' text={skill} />
+			))}
+		</div>
+	);
+};
+
+const LeadershipSkills: React.FC = () => {
+	return (
+		<div
+			data-test='leadership-skills'
+			className='flex flex-col gap-10 xl:flex-1'>
+			{skills.leadership.map((skill, index) => (
+				<SkillTip key={index} typeOfSkill='Leadership' text={skill} />
+			))}
+		</div>
+	);
+};
