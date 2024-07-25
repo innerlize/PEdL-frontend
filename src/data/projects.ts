863b0d93e8da809830d720d307d761f688ed4c5b
innerlize <alan.dev@outlook.kr>
2024-07-24 22:45:08 -0300

    Add temporary 'projects' data file for UI development

---
+++ src/data/projects.ts
@@ -0,0 +1,37 @@
+export const projects = [
+	{
+		id: '183we1',
+		title: 'Project Title',
+		thumbnail: 'https://via.placeholder.com/250'
+	},
+	{
+		id: '324dw2e',
+		title: 'Project Title',
+		thumbnail: 'https://via.placeholder.com/250'
+	},
+	{
+		id: 'fdsa32',
+		title: 'Project Title',
+		thumbnail: 'https://via.placeholder.com/250'
+	},
+	{
+		id: 'df4rfsr',
+		title: 'Project Title',
+		thumbnail: 'https://via.placeholder.com/250'
+	},
+	{
+		id: 'dkvscdsa',
+		title: 'Project Title',
+		thumbnail: 'https://via.placeholder.com/250'
+	},
+	{
+		id: '2t4r3r',
+		title: 'Project Title',
+		thumbnail: 'https://via.placeholder.com/250'
+	},
+	{
+		id: 'fdgthrw3',
+		title: 'Project Title',
+		thumbnail: 'https://via.placeholder.com/250'
+	}
+];
