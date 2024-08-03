cc29e1dd5f492e2bef3a7bbe963ca8aa7d339c76
innerlize <alan.dev@outlook.kr>
2024-08-02 23:41:53 -0300

    Add ESLint configuration for 'TanStack Query' dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 
 export default [
 	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
@@ -9,6 +10,7 @@ export default [
 	{ languageOptions: { globals: globals.browser } },
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
+	...pluginQuery.configs['flat/recommended'],
 	pluginReactConfig,
 	{ settings: { react: { version: 'detect' } } },
 	{
