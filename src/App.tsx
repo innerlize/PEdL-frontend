897075c2300debe9419b01ac01c3b56f1982e1b3
innerlize <alan.dev@outlook.kr>
2024-08-02 23:51:59 -0300

    Initialize 'QueryClient' instance

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import TextAreaProvider from './contexts/TextAreaContext';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/routes';
 
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			retry: false
+		}
+	}
+});
 
 function App() {
 	return (
+		<QueryClientProvider client={queryClient}>
 			<TextAreaProvider>
 				<RouterProvider router={router} />
 			</TextAreaProvider>
+		</QueryClientProvider>
 	);
 }
 
