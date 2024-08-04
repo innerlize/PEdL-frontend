81a2d6525bc9b0e6d353953c42122bd7c6ae04bf
innerlize <alan.dev@outlook.kr>
2024-08-03 21:51:08 -0300

    Connect API to frontend in 'Portfolio' page

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,7 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
 import { ProjectsContainer } from '../components/Portfolio/ProjectsContainer';
 import { SectionHeader } from '../components/SectionHeader';
+import { getProjects } from '../api/projects';
+import { Spinner } from '../components/Spinner';
 
 const PortfolioPage: React.FC = () => {
+	const {
+		data: projects,
+		isLoading,
+		error
+	} = useQuery({
+		queryKey: ['projects'],
+		queryFn: getProjects
+	});
+
 	const title = (
 		<>
 			My Creative <span className='text-primary'>Showcase</span>
@@ -18,6 +30,30 @@ const PortfolioPage: React.FC = () => {
 			details!
 		</>
 	);
+
+	const renderContent = () => {
+		if (isLoading) {
+			return (
+				<div className='size-full flex justify-center items-center'>
+					<Spinner />
+				</div>
+			);
+		}
+
+		if (error) {
+			return <div>Error loading projects!</div>;
+		}
+
+		if (projects && projects.length > 0) {
+			return <ProjectsContainer projects={projects} />;
+		}
+
+		return (
+			<div className='size-full flex justify-center items-center text-2xl text-center italic'>
+				No projects found... <span className='mx-2 text-primary'>yet</span> ;)
+			</div>
+		);
+	};
 
 	return (
 		<div className='flex flex-col w-full'>
