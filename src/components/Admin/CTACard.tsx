e2fa8aa33768613fd61d4e01a3f3fb7a31ff528f
innerlize <alan.dev@outlook.kr>
2024-08-22 20:54:11 -0300

    Create 'CTACard' component

---
+++ src/components/Admin/CTACard.tsx
@@ -0,0 +1,37 @@
+interface CTACardProps {
+	title: string;
+	subtitle: string;
+	icon: JSX.Element;
+	backgroundColor: string;
+	hoverBackgroundColor: string;
+}
+
+export const CTACard: React.FC<CTACardProps> = ({
+	title,
+	subtitle,
+	icon,
+	backgroundColor,
+	hoverBackgroundColor
+}) => {
+	return (
+		<div
+			data-test='admin-CTACard'
+			className={`flex items-center justify-center gap-5 px-[28px] py-[30px] ${backgroundColor} rounded-[3px] cursor-pointer transition-color hover:${hoverBackgroundColor} md:max-xl:w-[470px] md:max-xl:px-[58px] md:py-[40px] 2xl:p-[58px]`}>
+			<div className='flex flex-col gap-2.5 w-[191px] text-left md:w-[243px] 2xl:gap-3'>
+				<p className='text-2xl font-black md:max-xl:text-4xl 2xl:text-[32px]'>
+					{title}
+				</p>
+
+				<p className='text-[15px] leading-[18px] md:text-base md:leading-[19px]'>
+					{subtitle}
+				</p>
+			</div>
+
+			<div
+				data-test='admin-CTACard-icon'
+				className='size-20 md:size-[90px] xl:max-2xl:size-16'>
+				{icon}
+			</div>
+		</div>
+	);
+};
