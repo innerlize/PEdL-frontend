2394f8f71b0b87b1b2a8e9475083094997d69221
innerlize <alan.dev@outlook.kr>
2024-09-01 20:23:27 -0300

    Modify message template for contact form

--- cypress/tests/ui/contact-page.spec.ts
+++ cypress/tests/ui/contact-page.spec.ts
@@ -57,7 +57,7 @@ describe('Contact Page', () => {
 		cy.get('button').contains('Use template').click();
 		cy.get('textarea').should(
 			'have.value',
-			"Hi [Recipient's Name], I hope you are doing well. I would like to propose a collaboration on [Project Name] that I believe we could both benefit from. Based on your skills and experience at [Recipient's Company], I think we would make a great team to achieve the goals of this project. I am confident that by working together, we can leverage our strengths and create something truly impactful. If you are interested, please let me know so we can discuss the details further. Looking forward to your response. Best regards, [Your Name] [Your Company]"
+			"Hi Pablo, I hope you are doing well. I would like to propose a collaboration on [Project Name] that I believe we could both benefit from. Based on your skills and experience at [Recipient's Company], I think we would make a great team to achieve the goals of this project. I am confident that by working together, we can leverage our strengths and create something truly impactful. If you are interested, please let me know so we can discuss the details further. Looking forward to your response. Best regards, [Your Name] [Your Company]"
 		);
 	});
 
