579436cd5d715b09c23a0fba43b2069c5e72fbae
innerlize <alan.dev@outlook.kr>
2024-09-01 20:22:13 -0300

    Create custom 'Projects Carousel' component for Admin Panel portfolio

---
+++ src/components/Admin/Portfolio/ProjectsCarousel.tsx
@@ -0,0 +1,50 @@
+import { FreeMode } from 'swiper/modules';
+import { Project } from '../../../types/Portfolio';
+import { AdminProjectCard } from './ProjectCard';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+interface AdminProjectsCarouselProps {
+	projects: Project[];
+}
+
+export const AdminProjectsCarousel: React.FC<AdminProjectsCarouselProps> = ({
+	projects
+}) => {
+	return (
+		<div className='mx-[-30px] md:mx-[-70px] xl:mx-[-127px] 2xl:mx-[-227px]'>
+			<Swiper
+				slidesPerView={'auto'}
+				spaceBetween={25}
+				breakpoints={{
+					0: {
+						slidesOffsetAfter: 30,
+						slidesOffsetBefore: 30
+					},
+					1280: {
+						slidesOffsetAfter: 127,
+						slidesOffsetBefore: 127
+					},
+					1536: {
+						slidesOffsetAfter: 227,
+						slidesOffsetBefore: 227
+					}
+				}}
+				modules={[FreeMode]}
+				freeMode
+				touchEventsTarget='container'>
+				{projects.map(project => (
+					<SwiperSlide
+						key={project.id}
+						className='w-[250px] h-[330px] md:w-[350px] md:h-[460px] xl:w-[220px] xl:h-[280px] 2xl:w-[340px] 2xl:h-[475px]'>
+						<AdminProjectCard
+							key={project.id}
+							id={project.id}
+							name={project.name}
+							thumbnail={project.thumbnail}
+						/>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</div>
+	);
+};
