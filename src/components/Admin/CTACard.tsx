1847c1570f47ca24447346bf9594dfbbbfcf0b48
innerlize <alan.dev@outlook.kr>
2024-09-17 17:15:52 -0300

    Add new route to 'CTACard' href

--- src/components/Admin/CTACard.tsx
+++ src/components/Admin/CTACard.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 interface CTACardProps {
 	title: string;
 	subtitle: string;
@@ -14,24 +16,26 @@ export const CTACard: React.FC<CTACardProps> = ({
 	hoverBackgroundColor
 }) => {
 	return (
-		<div
-			data-test='admin-CTACard'
-			className={`flex items-center justify-center gap-5 px-[28px] py-[30px] ${backgroundColor} rounded-[3px] cursor-pointer transition-color hover:${hoverBackgroundColor} md:max-xl:w-[470px] md:max-xl:px-[58px] md:py-[40px] 2xl:p-[58px]`}>
-			<div className='flex flex-col gap-2.5 w-[191px] text-left md:w-[243px] 2xl:gap-3'>
-				<p className='text-2xl font-black md:max-xl:text-4xl 2xl:text-[32px]'>
-					{title}
-				</p>
-
-				<p className='text-[15px] leading-[18px] md:text-base md:leading-[19px]'>
-					{subtitle}
-				</p>
-			</div>
-
+		<Link to={'/admin-panel/project/add'}>
 			<div
-				data-test='admin-CTACard-icon'
-				className='size-20 md:size-[90px] xl:max-2xl:size-16'>
-				{icon}
+				data-test='admin-CTACard'
+				className={`flex items-center justify-center gap-5 px-[28px] py-[30px] ${backgroundColor} rounded-[3px] cursor-pointer transition-color hover:${hoverBackgroundColor} md:max-xl:w-[470px] md:max-xl:px-[58px] md:py-[40px] 2xl:p-[58px]`}>
+				<div className='flex flex-col gap-2.5 w-[191px] text-left md:w-[243px] 2xl:gap-3'>
+					<p className='text-2xl font-black md:max-xl:text-4xl 2xl:text-[32px]'>
+						{title}
+					</p>
+
+					<p className='text-[15px] leading-[18px] md:text-base md:leading-[19px]'>
+						{subtitle}
+					</p>
+				</div>
+
+				<div
+					data-test='admin-CTACard-icon'
+					className='size-20 md:size-[90px] xl:max-2xl:size-16'>
+					{icon}
+				</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
