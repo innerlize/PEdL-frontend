5ed7ad8b692e6c7ad6c5e7a407f6249cbeadcf64
innerlize <alan.dev@outlook.kr>
2024-10-03 18:58:31 -0300

    Add 'logout' feature to 'Admin Navbar'

--- src/components/Admin/Navbar/Navbar.tsx
+++ src/components/Admin/Navbar/Navbar.tsx
@@ -1,13 +1,28 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../../hooks/useAuth';
 
 const AdminNavbar: React.FC = () => {
+	const { logout } = useAuth();
+	const navigate = useNavigate();
+
+	const handleLogout = async () => {
+		await logout();
+		navigate('/admin-panel/auth');
+	};
+
 	return (
-		<nav className='sticky flex items-center z-10 top-[30px] w-full h-full md:top-[70px] 2xl:top-[72px]'>
+		<nav className='sticky flex items-center justify-between z-10 top-[30px] w-full h-full font-roboto leading-[28px] text-white font-bold md:top-[70px] 2xl:top-[72px]'>
 			<Link
 				to='/admin-panel'
-				className='font-roboto px-[25px] py-[10px] leading-[28px] text-2xl text-white font-bold uppercase bg-primary rounded-[3px]'>
+				className='px-[25px] py-[10px] text-2xl uppercase bg-primary rounded-[3px]'>
 				Back
 			</Link>
+
+			<button
+				className='text-lg underline transition-colors hover:text-primary'
+				onClick={handleLogout}>
+				Logout
+			</button>
 		</nav>
 	);
 };
