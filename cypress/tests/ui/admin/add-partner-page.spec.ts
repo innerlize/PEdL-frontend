377a80c5e835b903633495b8bea88dc8f0b651df
innerlize <alan.dev@outlook.kr>
2024-10-19 01:59:11 -0300

    Add tests for 'update' feature in partner form

--- cypress/tests/ui/admin/add-partner-page.spec.ts
+++ cypress/tests/ui/admin/add-partner-page.spec.ts
@@ -1,9 +1,20 @@
+import {
+	partners,
+	partnerCreatedResponse,
+	partnerUpdatedResponse
+} from '../../../fixtures/partners';
+
 describe('Admin - Add Partner Page', () => {
 	beforeEach(() => {
 		cy.intercept('POST', 'api/admin/auth/verify-admin-access', {
 			statusCode: 200,
 			body: true
 		}).as('VERIFY_ADMIN_ACCESS');
+
+		cy.intercept('POST', '/api/partners', {
+			statusCode: 200,
+			body: partnerCreatedResponse
+		}).as('CREATE_PARTNER');
 
 		cy.signInWithGoogle();
 
@@ -24,6 +35,7 @@ describe('Admin - Add Partner Page', () => {
 		cy.get('.text-red-500')
 			.contains('Partner name is required')
 			.should('be.visible');
+
 		cy.get('.text-red-500')
 			.contains('Partner logo is required')
 			.should('be.visible');
@@ -46,6 +58,8 @@ describe('Admin - Add Partner Page', () => {
 			.and('eq', logoURL);
 
 		cy.get('form').submit();
+
+		cy.wait('@CREATE_PARTNER');
 
 		cy.get('.Toastify__toast')
 			.contains('Partner successfully created!')
@@ -147,5 +161,111 @@ describe('Admin - Add Partner Page', () => {
 
 			cy.get('[data-test="admin-pill"]').should('not.exist');
 		});
+	});
+});
+
+describe('Admin - Add Partner Page - Update Partner', () => {
+	beforeEach(() => {
+		cy.intercept('POST', 'api/admin/auth/verify-admin-access', {
+			statusCode: 200,
+			body: true
+		}).as('VERIFY_ADMIN_ACCESS');
+
+		cy.intercept('GET', '/api/partners', {
+			statusCode: 200,
+			body: partners
+		}).as('GET_PARTNERS');
+
+		cy.intercept('GET', '/api/partners/*', {
+			statusCode: 200,
+			body: partners[0]
+		}).as('GET_SINGLE_PARTNER');
+
+		cy.intercept('PATCH', '/api/partners/*', {
+			statusCode: 200,
+			body: partnerUpdatedResponse
+		}).as('UPDATE_PARTNER');
+
+		cy.signInWithGoogle();
+
+		cy.visit('/admin-panel/partners');
+
+		cy.wait('@VERIFY_ADMIN_ACCESS');
+
+		cy.wait('@GET_PARTNERS');
+
+		cy.get('[data-test="partner-card-1"]').should('be.visible').click();
+
+		cy.url().should(
+			'eq',
+			`${Cypress.config().baseUrl}/admin-panel/partners/add`
+		);
+
+		cy.wait('@GET_SINGLE_PARTNER');
+	});
+
+	it('should render the page with header and form', () => {
+		cy.get('h2').contains('Updating partner').should('be.visible');
+
+		cy.get('form').should('exist');
+	});
+
+	it('should populate form fields with existing partner data on load', () => {
+		const partner = partners[0];
+
+		cy.get('input[name="partnerName"]').should('have.value', partner.name);
+
+		cy.get('[data-test="admin-partner-name-field"] input').type(
+			'Example Partner'
+		);
+
+		cy.get('[data-test="admin-image-preview-box"] img')
+			.should('be.visible')
+			.and('have.attr', 'src')
+			.and('eq', partner.image);
+
+		cy.get('[data-test="links-pills-container"]')
+			.children()
+			.should('have.length', partner.links.length)
+			.each(($link, index) => {
+				cy.wrap($link)
+					.contains(partner.links[index].label)
+					.trigger('mouseover')
+					.then(() => {
+						cy.get('.react-tooltip')
+							.should('be.visible')
+							.and('contain', partner.links[index].src);
+					});
+			});
+	});
+
+	it('should update partner data when form is submitted', () => {
+		const newPartnerName = 'Updated Partner One';
+
+		cy.intercept('GET', '/api/partners/*', {
+			statusCode: 200,
+			body: {
+				...partners[0],
+				name: newPartnerName
+			}
+		}).as('GET_SINGLE_PARTNER_UPDATED');
+
+		cy.get('input[name="partnerName"]').clear().type(newPartnerName);
+
+		cy.get('form').submit();
+
+		cy.wait('@UPDATE_PARTNER')
+			.its('response.body.data.name')
+			.should('eq', newPartnerName);
+
+		cy.get('.Toastify__toast')
+			.contains('Partner successfully updated!')
+			.should('be.visible');
+
+		cy.reload();
+
+		cy.wait('@GET_SINGLE_PARTNER_UPDATED');
+
+		cy.get('input[name="partnerName"]').should('have.value', newPartnerName);
 	});
 });
