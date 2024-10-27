4fe749ab3b6339744682d54579c85e680b52eed1
innerlize <alan.dev@outlook.kr>
2024-10-26 23:12:18 -0300

    Modify styles for 'ProjectsTable' component

--- src/components/Admin/Portfolio/ProjectsTable.tsx
+++ src/components/Admin/Portfolio/ProjectsTable.tsx
@@ -9,6 +9,7 @@ import {
 import { useCallback, useEffect, useState } from 'react';
 import { useAuth } from '../../../hooks/useAuth';
 import { updateProjectOrder } from '../../../api/projects';
+import clsx from 'clsx';
 import { toast } from 'react-toastify';
 
 interface AdminProjectsTableProps {
@@ -29,6 +30,8 @@ export const AdminProjectsTable: React.FC<AdminProjectsTableProps> = ({
 		async (result: DropResult) => {
 			const { source, destination } = result;
 			if (!destination || source.index === destination.index) return;
+
+			const previousProjects = [...localProjects];
 
 			const reorderedProjects = [...localProjects];
 
@@ -53,6 +56,7 @@ export const AdminProjectsTable: React.FC<AdminProjectsTableProps> = ({
 				})
 				.catch(err => {
 					console.error('Error updating project order: ', err);
+					setLocalProjects(previousProjects);
 					toast.error(
 						`Error updating project order for "${app}"! Check the console for more details.`,
 						{ autoClose: 2000 }
@@ -67,33 +71,45 @@ export const AdminProjectsTable: React.FC<AdminProjectsTableProps> = ({
 	}, [projects, app]);
 
 	return (
-		<DragDropContext onDragEnd={handleOnDragEnd}>
-			<Droppable droppableId={`${app}-projects-order`}>
-				{provided => (
-					<ul ref={provided.innerRef} {...provided.droppableProps}>
-						{localProjects.map((project, index) => (
-							<Draggable
-								key={project.id}
-								draggableId={project.id}
-								index={index}>
-								{provided => (
-									<li
-										ref={provided.innerRef}
-										{...provided.draggableProps}
-										{...provided.dragHandleProps}>
-										<AdminProjectCard
-											id={project.id}
-											name={project.name}
-											thumbnail={project.thumbnail}
-										/>
-									</li>
-								)}
-							</Draggable>
-						))}
-						{provided.placeholder}
-					</ul>
-				)}
-			</Droppable>
-		</DragDropContext>
+		<div className='flex flex-col gap-4 justify-center items-center'>
+			<h2
+				className={clsx(
+					'text-sm font-bold text-center uppercase mb-4 tracking-widest md:text-lg lg:text-xl 2xl:text-2xl',
+					app === 'pedl' ? 'text-primary' : 'text-secondary'
+				)}>
+				{app} Projects
+			</h2>
+
+			<DragDropContext onDragEnd={handleOnDragEnd}>
+				<Droppable droppableId={`${app}-projects-order`}>
+					{provided => (
+						<ul ref={provided.innerRef} {...provided.droppableProps}>
+							{localProjects.map((project, index) => (
+								<Draggable
+									key={project.id}
+									draggableId={project.id}
+									index={index}>
+									{provided => (
+										<li
+											className='mb-4'
+											ref={provided.innerRef}
+											{...provided.draggableProps}
+											{...provided.dragHandleProps}>
+											<AdminProjectCard
+												id={project.id}
+												name={project.name}
+												thumbnail={project.thumbnail}
+												app={app}
+											/>
+										</li>
+									)}
+								</Draggable>
+							))}
+							{provided.placeholder}
+						</ul>
+					)}
+				</Droppable>
+			</DragDropContext>
+		</div>
 	);
 };
