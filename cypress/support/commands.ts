ab3d48774c1697679cc02696f5233b2189e8f2ce
innerlize <alan.dev@outlook.kr>
2024-10-30 23:00:11 -0300

    Create custom 'dragAndDrop' Cypress command for UI interactions

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -15,3 +15,51 @@ Cypress.Commands.add('signInWithGoogle', async () => {
 			console.error('Unexpected error: ', err);
 		});
 });
+
+Cypress.Commands.add('dragAndDrop', (subject: string, target) => {
+	Cypress.log({
+		name: 'DRAGNDROP',
+		message: `Dragging element "${subject}" to the position of element "${target}"`,
+		consoleProps: () => {
+			return {
+				subject: subject,
+				target: target
+			};
+		}
+	});
+
+	const BUTTON_INDEX = 0;
+	const SLOPPY_CLICK_THRESHOLD = 10;
+
+	cy.get(target)
+		.first()
+		.then($target => {
+			const coordsDrop = $target[0].getBoundingClientRect();
+			cy.get(subject)
+				.first()
+				.then(subject => {
+					const coordsDrag = subject[0].getBoundingClientRect();
+					cy.wrap(subject)
+						.trigger('mousedown', {
+							button: BUTTON_INDEX,
+							clientX: coordsDrag.x,
+							clientY: coordsDrag.y,
+							force: true
+						})
+						.trigger('mousemove', {
+							button: BUTTON_INDEX,
+							clientX: coordsDrag.x + SLOPPY_CLICK_THRESHOLD,
+							clientY: coordsDrag.y,
+							force: true
+						});
+					cy.get('body')
+						.trigger('mousemove', {
+							button: BUTTON_INDEX,
+							clientX: coordsDrop.x,
+							clientY: coordsDrop.y,
+							force: true
+						})
+						.trigger('mouseup');
+				});
+		});
+});
--- cypress/support/cypress.d.ts
+++ cypress/support/cypress.d.ts
@@ -1,5 +1,6 @@
 declare namespace Cypress {
 	interface Chainable {
 		signInWithGoogle: () => void;
+		dragAndDrop: (subject: string, target: string) => Chainable<Element>;
 	}
 }
