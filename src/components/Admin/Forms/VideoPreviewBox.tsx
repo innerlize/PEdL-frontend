79ba53dd7625c85515b700c831bff5bff21b7af4
innerlize <alan.dev@outlook.kr>
2024-11-12 22:37:13 -0300

    Apply minor style changes to 'video preview' box

--- src/components/Admin/Forms/VideoPreviewBox.tsx
+++ src/components/Admin/Forms/VideoPreviewBox.tsx
@@ -10,14 +10,14 @@ export const AdminVideoPreviewBox: React.FC<AdminVideoPreviewBoxProps> = ({
 	videoSrc,
 	removeFn
 }) => (
-	<div data-test='admin-video-preview-box' className='relative size-24'>
+	<div data-test='admin-video-preview-box' className='relative size-32'>
 		<div
-			className='absolute p-[2px] text-center top-[5px] right-[5px] text-white bg-warning cursor-pointer rounded-[3px] drop-shadow-lg transition-colors hover:bg-red-500'
+			className='absolute p-[2px] z-10 text-center top-[5px] right-[5px] text-white bg-warning cursor-pointer rounded-[3px] drop-shadow-lg transition-colors hover:bg-red-500'
 			onClick={removeFn}>
 			<IoIosRemoveCircle className='size-full' />
 		</div>
 
-		<div className='size-full rounded-[3px] outline outline-1 outline-secondary object-cover cursor-zoom-in'>
+		<div className='size-full rounded-[3px] outline outline-1 outline-primary object-cover overflow-hidden'>
 			<ReactPlayer
 				width={'100%'}
 				height={'100%'}
